bffab08fb97ee753abd1e7804cf5bcfbfd2370e3
Gilles GAUTIER <ggautier@b2pweb.com>
2021-07-29 17:55:01 +0200

    chore: prepare project environment

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+};
